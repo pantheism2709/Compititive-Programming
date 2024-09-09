26a78a2af9e4610707bde0acdab9c18edda12c76
Sachin Mishra <96466673+pantheism2709@users.noreply.github.com>
2024-09-09 15:53:06 +0530

    maths

--- Algebra/MathsForCpTillPurple/CountNoOfFactors.cpp
+++ Algebra/MathsForCpTillPurple/CountNoOfFactors.cpp
@@ -30,3 +30,40 @@ int main() {
 
     return 0;
 }
+
+
+
+// count number of factors for all number till n
+// in optimal way ... the above algo take something
+// near O(n*sqrt(n)) which is quite more so we'll see
+// some more optimal way
+
+// Instead of traversing on each multiple because here we are waisting some iteration where number is not 
+// factor of dividend ... we wil traverse on multiple like ... if we are at 7 then 7 will be the divisor
+// for 14,21,28,35 ...etc. so we will increase the cntFactor of 7's multiples 
+
+// TC is less than O(n*log(n)) and O(n*log(n)) <<<<<< O(n*sqrt(n))
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+   
+   vector<int>cntFactor(n+1);
+                  
+    for(int i=1; i<=n;i++){
+     for(int j=i;j<=n;j+=i)
+     {
+         cntFactor[j]++;  // i is a divisor / factor of j
+     }
+    }
+  
+ for(int i=1;i<=n;i++)
+  {
+      cout<<cntFactor[i]<<endl;
+  }
+
+    return 0;
+}
